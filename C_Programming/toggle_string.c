dff84c4e2c66342193ff9fa69cbb01a0c3759cf7
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-04-08 22:33:01 +0530

    Created Toggle String program
    Change Uppercase letter to lowercase letter and vice-versa of a given string.

---
+++ C_Programming/toggle_string.c
@@ -0,0 +1,18 @@
+//Toggle String: Change Uppercase letter to lowercase letter and vice-versa of a given string.
+
+#include <stdio.h>
+ 
+int main() {
+    int ch;
+    printf("Input String: \n");
+    while(ch=getchar()) {
+        if(ch >= 'A' & ch <= 'Z')
+            ch += 32;
+        else if(ch >= 'a' & ch <= 'z')
+            ch -= 32;
+        putchar(ch);
+    }
+    return 0;
+}
+
+//If input is given from keyboard then use "ctrl+c" to terminate the program.
