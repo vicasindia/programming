26d6bc900282a563b85b60836169a0def92f5177
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-04-30 23:41:45 +0530

    English Alphabet

---
+++ C_Programming/english_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+	printf("English Alphabet: \n\n");
+	
+	for(char i = 'A'; i <= 'Z'; i++)
+	    printf(" %c ", i);
+	    
+	printf("\n\n------------------------------------------------------------------------------\n\n");
+	    
+	for(int i = 'a'; i <= 'z'; i++)
+	    printf(" %c ", i);
+	    
+	return 0;
+}
