e9916ab005505bf45b66b0464f6f6387d10074f1
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-05-04 23:21:33 +0530

    Factorial

--- C_Programming/find_factorial.c
+++ C_Programming/find_factorial.c
@@ -1,14 +1,17 @@
 // n! = n*(n-1)*(n-2)*.......3*2*1
+// Constraint: 0 <= n <= 20
 
 #include <stdio.h>
 
 int main() {
 	int num;
 	unsigned long long fact = 1;
-  printf("Enter the number to find factorial: ");
+    printf("Enter the number to find factorial: ");
 	scanf("%d", &num);
+	
 	for(int i=num; i>=2; i--)
 		fact *= i;
+	
 	printf("%d! = %llu", num, fact);
 	return 0;
 }
