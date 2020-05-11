5d0cf2b7a216fd6bb23276740ed8b925cfab6032
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-05-11 23:21:46 +0530

    Palindrome (String)

--- C_Programming/check_palindrome.c
+++ C_Programming/check_palindrome.c
@@ -1,18 +1,26 @@
+//A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward, e.g ABA, madam.
 // Program to check whether given string is Palindrome or Not
+//Constraint: The string should lowercase or UPPERCASE
+
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+int main() {
+	
 	char s[100];
 	int i, j, flag = 1;
+	
 	printf("Enter the String: ");
 	scanf("%s", s);
-	for(i=0, j=strlen(s)-1; i<j; i++, j--){
-		if(s[i] != s[j]){
+	int len = strlen(s);
+	
+	for(i=0, j=len-1; i<j; i++, j--) {
+		if(s[i] != s[j]) {
 			flag=0;
 			break;
 		}	
 	}
 	flag ? printf("Congratulations! It\'s a Palindrome.") : printf("Oops! Not Palindrome. Try another.");
+	
 	return 0;
 }
