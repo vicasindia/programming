36917bfbce0d2a674fed2935744dcec940868665
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-05-20 21:42:58 +0530

    Fahrenheit to Celsius conversion

---
+++ C_Programming/fahrenheit_to_celsius.c
@@ -0,0 +1,17 @@
+//Program for Fahrenheit to Celsius conversion
+
+#include <stdio.h>
+
+int main() {
+    
+    float fahr, celsius;
+    printf("Enter the temperature in Fahrenheit(°F): ");
+    scanf("%f", &fahr);
+    
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    
+    printf("\nTemperature in Celsius: %.2f °C", celsius);
+	
+	return 0;
+}
+
