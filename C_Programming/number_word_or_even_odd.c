3bf70005064d37580f3f54edf30df1c1397c66c0
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-05-22 23:54:16 +0530

    Number to Word or Print Even-Odd

--- C_Programming/number_word_or_even_odd.c
+++ C_Programming/number_word_or_even_odd.c
@@ -1,10 +1,10 @@
-/*
+/************************************************************
 For each integer n in the interval  [a,b](given as input):
     If 0<=n<=9, print the English lowercase representation.
     Else if n>9 and  even number, print "even".
     Else if n>9 and  odd number, print "odd".
                                         -HackerRank
-*/
+*************************************************************/
 
 //Input-Output, 2D Array, Pointer, If-Else, Loop
 
@@ -12,14 +12,17 @@ For each integer n in the interval  [a,b](given as input):
 
 int main() 
 {
+    
     int a, b;
     printf("Enter lower and upper range: ");
     scanf("%d %d", &a, &b);
+    
     char word[][10] = {"zero","one","two","three","four","five","six","seven","eight","nine"}; //char *word[10]
     printf("----------------------------------\n");
+    
     for(int i=a; i<=b; i++) {
-        if(i>9) {
-            if(i%2 == 0)
+        if(i > 9) {
+            if(!(i % 2))
                 printf("even\n");
             else
                 printf("odd\n");
@@ -27,5 +30,6 @@ int main()
         else
             printf("%s\n", word[i]);
     }
+    
     return 0;
 }
