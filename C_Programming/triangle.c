382975dd1fbfc507f88731bec8879fa6366a5bbb
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-05-24 16:20:07 +0530

    Triangle △

---
+++ C_Programming/triangle.c
@@ -0,0 +1,36 @@
+/***************************************************************************
+ Triangle: A triangle is a polygon with three edges and three vertices.
+  It is one of the basic shapes in geometry. 
+  A triangle with vertices A, B, and C is denoted △ABC.
+ 
+                            A
+                            /\
+                           /  \
+                          /____\
+                         B      C
+****************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+	
+	double base, height, perimeter, area;
+	printf("Enter the base and height of the triangle: ");
+	scanf("%lf %lf", &base, &height);
+	
+	printf("\n\n\t--------------- TRIANGLE ---------------");
+	printf("\nA triangle is a polygon with three edges and three vertices.");
+	printf("\n\nNo. of vertices: 3");
+	printf("\nNo. of edges: 3");
+	printf("\nsum of all internal angles: 180°");
+	printf("\nPerimeter: sum of side lengths");
+	printf("\nBase: %.2lf", base);
+	printf("\nHeight: %.2lf", height);
+	
+	area = 0.5 * base * height;
+	
+	printf("\nArea: %.2lf", area);
+	
+	return 0;
+}
+
