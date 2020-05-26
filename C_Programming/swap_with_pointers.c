32881adfae0394b06780cd0ec98ba0a590b9bbb2
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-05-26 19:09:38 +0530

    Swap using Pointer

--- C_Programming/swap_with_pointers.c
+++ C_Programming/swap_with_pointers.c
@@ -1,8 +1,9 @@
-//Swapping two numbers using pointers
+//Swapping two numbers using pointer
 
 #include <stdio.h>
 
-long int  swap(long int*,long int* );
+long swap(long*, long*);
+
 int main(void) {
     
 	long num1, num2;
@@ -12,18 +13,18 @@ int main(void) {
 	
 	printf("\nBefore Swapping: Num1 = %ld, Num2 = %ld", num1, num2);
 	
-	swap(&num1,&num2);
+	swap(&num1, &num2);
 	
 	printf("\nAfter Swapping:  Num1 = %ld, Num2 = %ld", num1, num2);
 	
 	return 0;
 }
-long int swap(long int *a,long int *b){
+
+long swap(long *a, long *b) {
+	
     long temp;
-    temp=*b;
-    *b=*a;
-    *a=temp;
-    
-}
-        
+    temp = *b;
+    *b = *a;
+    *a = temp;   
+}       
 
