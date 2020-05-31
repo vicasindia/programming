53274c6a646c6041d1a18cb6422f102e40ef7ed3
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-05-31 16:25:50 +0530

    Check Prime Number

--- C_Programming/check_prime_number.c
+++ C_Programming/check_prime_number.c
@@ -1,27 +1,42 @@
-/*
-A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself. 
+/**********************************************************************************************************
+A prime number is a natural number (greater than 1) that has no positive divisors other than 1 and itself. 
 The first few prime numbers are {2, 3, 5, 7, 11, ….}
         - GeeksforGeeks
-*/
+************************************************************************************************************/
 
 #include <stdio.h>
 #include <math.h>
 
 int main(void) {
-	int num, i, flag=1;
+	
+	long num; 
+	int i, flag=1;
+	
 	printf("Enter a number: ");
-	scanf("%d", &num);
-	for(i = 2; i <= sqrt(num); i++) {
-	    if(num % i == 0) {
-	        flag = 0;
-	        break;
-	    }
+	scanf("%ld", &num);
+
+	if(num == 2) {			//2 is Prime number
+
+	}			
+
+	else if(num < 2 || !(num % 2))		//Even numbers (except 2) can't be Prime numbers
+		flag = 0;
+
+	else {
+		for(i = 3; i <= sqrt(num); i += 2) {
+		    
+		    if(!(num % i)) {
+		        flag = 0;
+		        break;
+		    }
+		}
 	}
 	
+	
 	if(flag)
-	    printf("%d is a Prime Number.", num);
+	    printf("%ld is a Prime Number.", num);
 	else
-		printf("%d is not a Prime Number.", num);	    
+        printf("%ld is not a Prime Number.", num);	    
 	    
 	return 0;
 }
