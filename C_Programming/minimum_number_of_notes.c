e2da1bec6f1a543efa3254b725d457fe2f37ffc0
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-06-03 22:13:07 +0530

    Minimum numbers of Notes

--- C_Programming/minimum_number_of_notes.c
+++ C_Programming/minimum_number_of_notes.c
@@ -1,19 +1,23 @@
-/*
-Consider a currency system in which there are notes of nine denominations, namely, ₹1, ₹2, ₹5, ₹10, ₹20, ₹50, ₹100, ₹500, ₹2000.
+/**************************************************************************
+Consider a currency system in which there are notes of nine denominations, 
+namely, ₹1, ₹2, ₹5, ₹10, ₹20, ₹50, ₹100, ₹500, ₹2000.
 Program to compute minimum number of notes that will combine to give ₹ N.
-				-CodeChef
-*/
+											-CodeChef
+***************************************************************************/
 
 #include <stdio.h>
 
 int main(void) {
+	
 	unsigned long long int amount;
-	int i=0, count=0;
+	
 	int currency[] = {2000, 500, 100, 50, 20, 10, 5, 2, 1};
+	int i = 0, count = 0;
+	
 	printf("Enter the amount: ₹");
 	scanf("%llu", &amount);
 	    
-	while(amount && i<9) {
+	while(amount && i < 9) {
 		int notes=currency[i];
 		if(amount >= notes) {
 			amount -= notes;
@@ -22,6 +26,8 @@ int main(void) {
 		else
 			i++;
 	}
+	
 	printf("Minimum numbers of Notes: %d\n", count);
+	
 	return 0;
 }
