86b2518720b3a548ee6f123cd47dc34b65196528
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-06-05 20:33:31 +0530

    Control Flow: if statement

---
+++ C_Programming/if_statement.c
@@ -0,0 +1,16 @@
+//Control flow: if statement
+
+#include <stdio.h>
+
+int main() {
+
+	int num = 10;	//Initialization
+
+	if(num < 20) {	//Checking condition
+
+		printf("%d is less than 20", num);	//Printing if condition is true
+	}
+
+	return 0;
+}
+
