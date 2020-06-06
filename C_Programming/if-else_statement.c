527b1103c3267a5c0682d725a59719504c2e2591
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-06-06 22:16:26 +0530

    Control flow: if-else statement

---
+++ C_Programming/if-else_statement.c
@@ -0,0 +1,20 @@
+//Control flow: if-else statement
+
+#include <stdio.h>
+
+int main() {
+
+	int num = 30;	//Initialization
+
+	if(num < 20) {	//Checking condition
+
+		printf("%d is less than 20", num);	//Printing if condition is true
+	}
+	else {
+		
+		printf("%d is greater than 20", num);	//Printing if condition is false
+	}
+
+	return 0;
+}
+
