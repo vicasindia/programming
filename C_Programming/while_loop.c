9ae71b691cfa3f8a3131cbcd62458b377a738018
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-07-17 22:43:36 +0530

    Control flow: while loop

---
+++ C_Programming/while_loop.c
@@ -0,0 +1,16 @@
+//Control Flow: while loop
+
+#include <stdio.h>
+
+int main()
+{
+	int a = 10;	//Initialization
+
+	while(a < 20)	//Test condition
+	{
+		printf("a value: %d\n", a);
+		a++;	//Update expression
+	}
+	return 0;
+}
+
