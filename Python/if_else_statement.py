a16e89483170f1bbc2a35f7257770f031aa6e08b
lalit singh <33358596+lucken99@users.noreply.github.com>
2020-10-03 01:01:01 +0530

    * [if-else statement](Python/if-else_statement.py)

---
+++ Python/if_else_statement.py
@@ -0,0 +1,7 @@
+# Control flow: if-else statement
+
+number = 40
+if number < 20:
+	print(number, "is less than 20")	# Printing if condition is true
+else:
+	print(number, "is greater than or equal to 20") # Printing if condition is false
