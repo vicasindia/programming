fa9474ee4bd8da3dc89da320a937c9ce578adf2b
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-10-03 01:20:16 +0530

    if elif else statement

--- Python/if_else_if_statement.py
+++ Python/if_else_if_statement.py
@@ -3,8 +3,10 @@
 number = 30
 if number < 20:	# checking condition
 	print(number ,"is less than 20")
+	
 elif number < 100:
 	print(number ,"is between 20 and 100 inclusive")	# executes if, if condition is false
-											# and elif condition is true
+								# and elif condition is true
 else:
 	print(number ,"is greater than 100") 
+	
