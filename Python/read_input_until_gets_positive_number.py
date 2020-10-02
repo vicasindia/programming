22f4d4a50b7c243648bc6cc708d129b19ca1f374
lalit singh <33358596+lucken99@users.noreply.github.com>
2020-10-03 01:02:29 +0530

    * [Read Input until user enters a positive number](Python/read_input_until_enter_positive_number.py)

---
+++ Python/read_input_until_gets_positive_number.py
@@ -0,0 +1,8 @@
+# Program to read input until user enter a positive number
+# e.g. -5 -16 0 61
+
+while True:
+	number = int(input("Enter a number: "))
+	if number > 0:
+		print("Positive number is", number)
+		break
