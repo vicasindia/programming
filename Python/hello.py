9aa0b3ba4cd76115ebc2bd4fbfcdf72260aa68f9
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-10-02 22:22:17 +0530

    Hello

---
+++ Python/hello.py
@@ -0,0 +1,26 @@
+# variable declaration
+name = ""
+year = 0
+
+# print on console
+print("Your good name: ")
+
+# input from console
+name = input()
+
+print("You are programming since (year): ")
+
+year = int(input())
+
+print("------------------------------------------")
+
+print("Hello {},\nWelcome to the \"programming\" repository.".format(name))
+
+year = 2020 - year
+
+print("You have {} year experience in programming.\nYour contribution will be very useful...\nHappy Coding!".format(year))
+
+'''***********************************************
+    author: Vikas Yadav
+    date: 2nd October,2020
+***********************************************'''
