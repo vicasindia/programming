6df3ccfa96e2c96942737e094e39d6b57f254096
lalit singh <33358596+lucken99@users.noreply.github.com>
2020-10-05 15:02:41 +0530

    Added more Python version of C programs

---
+++ Python/binary_converter.py
@@ -0,0 +1,15 @@
+#Input: Decimal Number (Integer)
+#Output: Binary Number (Signed 2's Complement)
+
+num = int(input("Enter the Decimal Number: "))
+byt = int(input("Enter the number of bytes u want to your integer be in: "))
+bit = byt*8
+
+for i in range(bit):
+	if (num<<i) & (1<<(bit-1)):
+		print(1,end="")
+	else:
+		print(0,end="")
+print()
+
+
