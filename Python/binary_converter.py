565500dae8d3f314dc43090a98e33fe9a5aa8034
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-10-10 20:00:23 +0530

    Binary Converter

--- Python/binary_converter.py
+++ Python/binary_converter.py
@@ -2,14 +2,13 @@
 #Output: Binary Number (Signed 2's Complement)
 
 num = int(input("Enter the Decimal Number: "))
-byt = int(input("Enter the number of bytes u want to your integer be in: "))
+byt = int(input("Enter the number of bytes you want to your integer be in: "))
 bit = byt*8
 
 for i in range(bit):
 	if (num<<i) & (1<<(bit-1)):
-		print(1,end="")
+		print(1, end="")
 	else:
-		print(0,end="")
+		print(0, end="")
 print()
-
 
