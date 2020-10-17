36081f8615adfa07e5c4c8d189361de251756daa
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-10-17 19:42:16 +0530

    Time Conversion

--- Python/time_conversion.py
+++ Python/time_conversion.py
@@ -1,19 +1,18 @@
 """
 Given a time in 12-hour AM/PM format,
 convert it to military (24-hour) time.
+
 Note:
-Midnight is 12:00:00AM on a 12-hour clock,
-and 00:00:00 on a 24-hour clock. 
-Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock.
+	Midnight is 12:00:00AM on a 12-hour clock,
+	and 00:00:00 on a 24-hour clock. 
+	Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock.
 
 Input - a string hh:mm:ssAM or hh:mm:ssPM (01 <= hh <= 12; 00 <= mm, ss <= 59)
 
 Output - hh:mm:ss (00 <= hh <= 23)
 
-                                    -HackerRank
-
+                           -HackerRank
 """
-
 
 def timeConversion(s):
     if s[-2] == 'A':
@@ -24,8 +23,6 @@ def timeConversion(s):
         if s[:2] == '12':
             return s[:-2]
         ans = str(int(s[:2])+12)
-		
-
 
 if __name__ == '__main__':
 
@@ -36,4 +33,3 @@ if __name__ == '__main__':
 	except:
 		print("please enter right time, e.g., 08:08:05PM (put leading zeros for single digit).")
 	
-
