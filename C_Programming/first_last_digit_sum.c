06166bc052e912d1a2d8f6280a0171fe84984d47
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-12-13 23:44:30 +0530

    Update first_last_digit_sum.c

--- C_Programming/first_last_digit_sum.c
+++ C_Programming/first_last_digit_sum.c
@@ -2,13 +2,16 @@
 
 #include <stdio.h>
 
-int main() {
-	unsigned long long int num;
+int main()
+{
+	unsigned long long num;
 	printf("Enter a number: ");
 	scanf("%llu", &num);
+	
 	int last = num % 10;
 	while(num > 10)
 		num /= 10;
 	printf("Sum of first and last digit: %d", num + last);
+	
 	return 0;
 }
