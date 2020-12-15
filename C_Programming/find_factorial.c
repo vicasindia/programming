e0e3ab3dd1082f10840547c474db389d0ec052d2
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2020-12-15 23:07:14 +0530

    Update find_factorial.c

--- C_Programming/find_factorial.c
+++ C_Programming/find_factorial.c
@@ -3,15 +3,15 @@
 
 #include <stdio.h>
 
-int main() {
-	
+int main()
+{	
 	int num;
 	unsigned long long fact = 1;
 	
     printf("Enter the number to find factorial: ");
 	scanf("%d", &num);
 	
-	for(int i=num; i>=2; i--)
+	for(int i = num; i >= 2; i--)
 		fact *= i;
 	
 	printf("%d! = %llu", num, fact);
