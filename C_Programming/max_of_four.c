b7e2ef558180bb2f222a1e4fb0be4b4a79ac3ace
Vikas Yadav <62963429+vicasindia@users.noreply.github.com>
2021-01-21 23:49:30 +0530

    Update max_of_four.c

--- C_Programming/max_of_four.c
+++ C_Programming/max_of_four.c
@@ -1,18 +1,23 @@
 // Find maximum number among 4 numbers.
 #include <stdio.h>
 
-int max_of_four(int a, int b, int c, int d) {
-    int max1, max2;
-    max1=a>b ? a : b;
-    max2=c>d ? c : d; 
-    return max1>max2 ? max1 : max2;
+long max_of_four(long a, long b, long c, long d)
+{
+    long max1, max2;
+    max1 = a > b ? a : b;
+    max2 = c > d ? c : d; 
+    
+    return max1 > max2 ? max1 : max2;
 }
-int main() {
-    int a, b, c, d;
+
+int main()
+{
+    long a, b, c, d;
     printf("Enter 4 numbers:\n");
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    int ans = max_of_four(a, b, c, d);
-    printf("Max is %d.", ans);   
+    scanf("%ld %ld %ld %ld", &a, &b, &c, &d);
+    long ans = max_of_four(a, b, c, d);
+    printf("Max is %ld.", ans);
+    
     return 0;
 }
 
